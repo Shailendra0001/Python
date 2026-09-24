0fdd29eead41de7c119625be36ae07bb6eac05e3
Shailendra Chauhan <shailendrachauhan11223344@gmail.com>
2026-09-24 07:35:27 +0530

    print +ve and -ve numbers in a list

---
+++ print+ve.py
@@ -0,0 +1,10 @@
+l = [13,-12,45,-34,-32,57]
+print("positive numbers")
+for i in range (len(l)):
+    if(l[i]>0):
+        print(l[i])
+   
+print("negative numbers")
+for i in range (len(l)):
+    if(l[i]<0):
+        print(l[i])
